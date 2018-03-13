2728bc42636bd1c27acf1319cf5cca6feef88e12
hepeng <hopehp108@163.com>
2018-03-13 20:48:42 +0800

    说明注解的@target含义

--- src/Mytag.java
+++ src/Mytag.java
@@ -4,6 +4,8 @@ import java.lang.annotation.*;
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Mytag {
+    //@target注解，表示该创建的注解可以用在哪些地方，本例子中声明的为ElementType.METHOD,ElementType.FIELD，即表示该
+    //Mytag标签可以用在方法和类的属性上面，用在其他地方则非法。
     String name() default "车";
     int size() default 10;
 }
